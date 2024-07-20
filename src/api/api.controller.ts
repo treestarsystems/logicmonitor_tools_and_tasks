43ce3d6a8b36d44724a5330c80aa928945b737fc
treestarsystems <51288647+treestarsystems@users.noreply.github.com>
2024-07-20 07:52:32 -0400

    testing api response validation and swagger documentation

--- src/api/api.controller.ts
+++ src/api/api.controller.ts
@@ -1,12 +1,7 @@
 import { Controller } from '@nestjs/common';
 import { Get } from '@nestjs/common';
 import { ApiService } from './api.service';
-import {
-  ApiDefaultResponse,
-  ApiOkResponse,
-  ApiResponse,
-  ApiTags,
-} from '@nestjs/swagger';
+import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { DefaultResponseObject } from 'src/utils/models.service';
 
 @Controller()
@@ -15,7 +10,7 @@ export class ApiController {
   constructor(private readonly appService: ApiService) {}
 
   @Get()
-  @ApiResponse({ type: DefaultResponseObject })
+  @ApiOkResponse({ type: DefaultResponseObject })
   getHello(): DefaultResponseObject {
     return this.appService.getHello();
   }
