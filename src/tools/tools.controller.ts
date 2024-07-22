b9d581de3035f8d3bdb3c92d1ccd9c624f764fa9
treestarsystems <51288647+treestarsystems@users.noreply.github.com>
2024-07-22 01:17:37 -0400

    remove unsused code and comments

--- src/tools/tools.controller.ts
+++ src/tools/tools.controller.ts
@@ -13,9 +13,7 @@ import { Get, Query } from '@nestjs/common';
 export class ToolsController {
   constructor(private readonly backupService: BackupService) {}
 
-  // Soruce for query DTO: https://tkssharma.com/nestjs-playing-with-query-param-dto/
-  // TODO: Customize response object for validation failures.
-  // - https://dev.to/nithinkjoy/how-to-use-class-validator-and-return-custom-error-object-in-nestjs-562h
+  // Source for query DTO: https://tkssharma.com/nestjs-playing-with-query-param-dto/
   @Get('backup/datasources')
   @ApiResponse({ type: ResponseObjectDefault })
   async backupDatasources(
@@ -31,25 +29,5 @@ export class ToolsController {
       // request,
       response,
     );
-    // try {
-    //   await this.backupService.backupDatasources(
-    //     query.company,
-    //     query.accessId,
-    //     query.accessKey,
-    //     query.searchString,
-    //     // request,
-    //     response,
-    //   );
-    // } catch (err) {
-    //   throw new HttpException(
-    //     {
-    //       status: 'failure',
-    //       httpStatus: 500,
-    //       message: err.message,
-    //       payload: [],
-    //     },
-    //     500,
-    //   );
-    // }
   }
 }
