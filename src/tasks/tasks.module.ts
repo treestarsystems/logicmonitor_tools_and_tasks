484b0d3f54d0dc7aad9ab9831e25a49737e83eb9
treestarsystems <51288647+treestarsystems@users.noreply.github.com>
2024-08-05 23:28:38 -0400

    laying foundation for tasks.

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,9 +1,35 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { TasksService } from './tasks.service';
 import { TasksController } from './tasks.controller';
-
+import { BackupServiceDatasources } from '../tools/tools-backup-datasources.service';
+import { BackupServiceGeneral } from '../tools/tools-backup-general.service';
+import { ToolsModule } from '../tools/tools.module';
+import { StorageServiceMongoDB } from 'src/storage/storage-mongodb.service';
+import { UtilsService } from 'src/utils/utils.service';
+import { StorageServiceZip } from '../storage/storage-zip.service';
+import {
+  BackupLMDataDatasource,
+  BackupLMDataGeneral,
+  BackupSchemaDatasource,
+  BackupSchemaGeneral,
+} from '../storage/schemas/storage-mongodb.schema';
 @Module({
-  providers: [TasksService],
-  controllers: [TasksController]
+  providers: [
+    TasksService,
+    BackupServiceDatasources,
+    BackupServiceGeneral,
+    UtilsService,
+    StorageServiceMongoDB,
+    StorageServiceZip,
+  ],
+  controllers: [TasksController],
+  imports: [
+    ToolsModule,
+    MongooseModule.forFeature([
+      { name: BackupLMDataDatasource.name, schema: BackupSchemaDatasource },
+      { name: BackupLMDataGeneral.name, schema: BackupSchemaGeneral },
+    ]),
+  ],
 })
 export class TasksModule {}
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
-
+import { BackupServiceDatasources } from '../tools/tools-backup-datasources.service';
+import { BackupServiceGeneral } from '../tools/tools-backup-general.service';
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  constructor(
+    private backupServiceDatasources: BackupServiceDatasources,
+    private backupServiceGeneral: BackupServiceGeneral,
+  ) {}
+
+  //   getTaskList(): string {
+  //     return 'This action returns all tasks';
+  //   }
+
+  async executeTaskBackups(taskId: string): Promise<void> {}
+}
